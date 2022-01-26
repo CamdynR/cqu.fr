1c0d87e93aad6b2dd01e032e91fa6a63d7ca285c
CamdynR <rasque.camdyn@gmail.com>
2022-01-26 12:37:51 -0800

    Initial URL shortener test

--- shortener.js
+++ shortener.js
@@ -4,25 +4,26 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-const shortened = {
-  test: 'https://google.com',
-};
+const shortened = {};
 
 app.get('/', (req, res) => {
-  const url = req.params.u;
-  if (!url) {
-    res.send('Hello World!');
-    return;
-  }
-  res.send(url);
+  res.send('Hello World!');
 });
 
 app.get('/:short', (req, res) => {
-  if (!req.params.short || !shortened[req.params.short]) {
-    res.status(404).send(`This URL does not exist: /${req.params.short}`);
+  const short = req.params.short;
+  if (short.length == 3 && shortened[short]) {
+    res.redirect(301, shortened[short]);
+    return;
+  } else if (short.length <= 3 && !shortened[short]) {
+    res.status(404).send(`This URL does not exist: /${short}`);
     return;
   } else {
-    res.redirect(301, shortened[req.params.short]);
+    const newShortCode = generateShortURL();
+    shortened[newShortCode] = short;
+    res.json({
+      url: `https://cqu.fr/${newShortCode}`
+    });
   }
 });
 
@@ -36,11 +37,16 @@ app.listen(port, () => {
 
 /**
  * Takes in a URL and creates a unique, short URL for it
- * @param {string} url The desired url to shorten
  * @returns {string} The new shortened 4 character path e.g. /mdpk
  */
-function generateShortURL(url) {
+function generateShortURL() {
+  const charSet = 'ABCDEFGHJKMNPQRSTUVWXYZ';
   let shortURL = '';
-  // TODO
+  while (Object.keys(shortened).contains(shortURL)) {
+    shortURL = '';
+    for (let i = 0; i < 3; i++) {
+      shortURL += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    }
+  }
   return shortURL;
 }
