b6187294ee76b9eb24a6c22db88e35c7184e6d59
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-01-26 09:52:15 -0800

    Bug fix attempt 1

--- shortener.js
+++ shortener.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 
 app.get('/:short', (req, res) => {
   if (!req.params.short || shortened[req.params.short]) {
-    res.status(404).send('This URL does not exist');
+    res.status(404).send(`This URL does not exist: ${req.params.short}`);
     return;
   } else {
     res.redirect('301', shortened[req.params.short]);
