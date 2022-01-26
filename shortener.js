35538d4adf21e40752c9f1419ec329dd3bd0fdd9
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-01-26 09:53:15 -0800

    Fixed bug in test short url

--- shortener.js
+++ shortener.js
@@ -18,8 +18,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:short', (req, res) => {
-  if (!req.params.short || shortened[req.params.short]) {
-    res.status(404).send(`This URL does not exist: ${req.params.short}`);
+  if (!req.params.short || !shortened[req.params.short]) {
+    res.status(404).send(`This URL does not exist: /${req.params.short}`);
     return;
   } else {
     res.redirect('301', shortened[req.params.short]);
