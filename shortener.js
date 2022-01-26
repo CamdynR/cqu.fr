8e07cc83e083082e0ee5e1f514bafb7742729366
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-01-26 09:50:21 -0800

    First initial shortener test

--- shortener.js
+++ shortener.js
@@ -2,14 +2,28 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+const shortened = {
+  test: 'https://google.com',
+};
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const url = req.params.u;
+  if (!url) {
+    res.send('Hello World!');
+    return;
+  }
+  res.send(url);
 });
 
 app.get('/:short', (req, res) => {
-  // TODO
+  if (!req.params.short || shortened[req.params.short]) {
+    res.status(404).send('This URL does not exist');
+    return;
+  } else {
+    res.redirect('301', shortened[req.params.short]);
+  }
 });
 
 app.listen(port, () => {
