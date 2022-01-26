7436cabc690e7e52c3355a1cac9e1c8c6918c1af
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-01-26 09:54:04 -0800

    Fixed redirect syntax

--- shortener.js
+++ shortener.js
@@ -22,7 +22,7 @@ app.get('/:short', (req, res) => {
     res.status(404).send(`This URL does not exist: /${req.params.short}`);
     return;
   } else {
-    res.redirect('301', shortened[req.params.short]);
+    res.redirect(301, shortened[req.params.short]);
   }
 });
 
