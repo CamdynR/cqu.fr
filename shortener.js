fcef1e91ee6367d06e9934f4a298b7cdd65f32ad
CamdynR <rasque.camdyn@gmail.com>
2022-01-26 20:44:43 -0800

    Fixed duplication issue

--- shortener.js
+++ shortener.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-const shortened = {};
+const keysToURLs = {};
+const urlsToKeys = {};
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -16,16 +17,22 @@ app.get('/favicon.ico', (req, res) => {
 
 app.get('/:short', (req, res) => {
   const short = req.params.short;
-  if (short.length == 3 && shortened[short]) {
-    res.redirect(301, shortened[short]);
+  if (short.length == 3 && keysToURLs[short]) {
+    res.redirect(301, keysToURLs[short]);
     return;
-  } else if (short.length <= 3 && !shortened[short]) {
+  } else if (short.length <= 3 && !keysToURLs[short]) {
     res.status(404).send(`This URL does not exist: /${short}`);
+    return;
+  } else if (urlsToKeys[short]) {
+    res.json({
+      url: `https://cqu.fr/${urlsToKeys[short]}`
+    });
     return;
   } else {
     const newShortCode = generateShortURL();
     console.log(newShortCode);
-    shortened[newShortCode] = short;
+    keysToURLs[newShortCode] = short;
+    urlsToKeys[short] = newShortCode;
     res.json({
       url: `https://cqu.fr/${newShortCode}`
     });
@@ -52,6 +59,6 @@ function generateShortURL() {
     for (let i = 0; i < 3; i++) {
       shortURL += charSet.charAt(Math.floor(Math.random() * charSet.length));
     }
-  } while (Object.keys(shortened).includes(shortURL));
+  } while (Object.keys(keysToURLs).includes(shortURL));
   return shortURL;
 }
